19ac2abf931491c4129f9f5351d325198386e77a
Ken Teng <kenktent@gmail.com>
2025-09-25 19:33:31 +0800

    test

--- api/test.js
+++ api/test.js
@@ -1,11 +1,3 @@
 export default function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-
-  res.status(200).json({ message: "✅ CORS test success" });
+  return res.status(200).json({ message: "API is working!" });
 }
